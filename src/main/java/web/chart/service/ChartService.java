d629276612cf7e4b54c2ea9ab6a0d56edc65a4f5
zhuanqiu <zhuanqiu84@gmail.com>
2026-02-02 14:34:19 +0800

    圖表頁面

---
+++ src/main/java/web/chart/service/ChartService.java
@@ -0,0 +1,7 @@
+package web.chart.service;
+
+import web.chart.bean.Chart;
+
+public interface ChartService {
+	
+}
---
+++ src/main/java/web/chart/service/impl/ChartServiceImpl.java
@@ -0,0 +1,5 @@
+package web.chart.service.impl;
+
+public class ChartServiceImpl {
+
+}
