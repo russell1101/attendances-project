1143611dcaf934c08e3109527ffec12ed966dad2
Marco <marco.pan1998@gmail.com>
2026-04-04 12:18:33 +0800

    登入修改

--- src/main/java/web/employee/dto/EmployeeDto.java
+++ src/main/java/web/employee/dto/EmployeeDto.java
@@ -1,5 +1,28 @@
 package web.employee.dto;
 
-public class EmployeeDto {
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Timestamp;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDto {
+	private Long employeeId;
+	private String name;
+	private String email;
+	private String googleSub;
+	private Date hireDate;
+	private BigDecimal currentPoints;
+	private String departmentName;
+	private String statusName;
+	private Boolean isActive;
+	private Timestamp createdAt;
+	private Timestamp updatedAt;
 }
