86eb38a0eec654260840c4ca748b3a03538997cf
Marco <marco.pan1998@gmail.com>
2026-04-16 20:15:31 +0800

    email登入網址更改

--- src/main/java/web/employee/util/EmailUtil.java
+++ src/main/java/web/employee/util/EmailUtil.java
@@ -14,7 +14,7 @@ public class EmailUtil {
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setTo(email);
 		message.setSubject("登入密碼通知");
-		String frontUrl = "http://127.0.0.1:5500/rci-login-front.html";
+		String frontUrl = "http://127.0.0.1/attendance-system/rci-login-front.html";
 		message.setText("請用以上密碼" + password + "於" + frontUrl + "登入您的帳號");
 
 		mailSender.send(message);
